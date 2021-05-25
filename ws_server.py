153f1152970e2304bff9d57786c828fb9414564d
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-05-25 18:58:28 -0500

    remove caching and add ability to get a particular ws state via API query param

--- ws_server.py
+++ ws_server.py
@@ -7,30 +7,26 @@ from eth2spec.phase0.spec import (
     MIN_PER_EPOCH_CHURN_LIMIT, MIN_VALIDATOR_WITHDRAWABILITY_DELAY,
     SAFETY_DECAY, SECONDS_PER_SLOT, SLOTS_PER_EPOCH
 )
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import redis
 import time
-import json
 import logging
 import yaml
 
 with open("/app/config.yml", "r") as config_file:
     cfg = yaml.safe_load(config_file)
 logging.basicConfig(format='%(asctime)s -- %(levelname)s -- %(message)s')
-logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.DEBUG)
 r = redis.Redis(host='redis')
 # ETH2_API is the Eth2 beacon node's HTTP endpoint
 ETH2_API = cfg["eth2_api"]
-# MIN_CACHE_UPDATE_EPOCHS is the minimum number of epochs between consecutive
-# updates of the cached weak subjectivity data
-MIN_CACHE_UPDATE_EPOCHS = int(cfg["min_cache_update_epochs"])
 # Optional graffiti to serve in the HTTP JSON response
 WS_SERVER_GRAFFITI = cfg["ws_server_graffiti"]
 
 
 def query_eth2_api(endpoint):
     url = ETH2_API + endpoint
-    response = httpx.get(url, timeout=100)
+    response = httpx.get(url, timeout=None)
     if response.status_code != 200:
         raise Exception(
             f"GET {url} returned with status code {response.status_code}"
@@ -152,12 +148,21 @@ def atomic_get_finalized_checkpoint_and_validator_info():
     return finalized_checkpoint, active_validator_count, avg_validator_balance
 
 
-def update_ws_data_cache():
+def get_ws_data(checkpoint = None):
     logging.info(f'Fetching weak subjectivity data from {ETH2_API}')
     finalized_checkpoint, active_validator_count, avg_validator_balance = \
         atomic_get_finalized_checkpoint_and_validator_info()
-    finalized_epoch = int(finalized_checkpoint["epoch"])
-    finalized_block_root = finalized_checkpoint["root"]
+    finalized_epoch = None
+    finalized_block_root = None
+    if checkpoint is None:
+        finalized_epoch = int(finalized_checkpoint["epoch"])
+        finalized_block_root = finalized_checkpoint["root"]
+    else:
+        # TODO: validate checkpointData
+        checkpointData = checkpoint.split(":")
+        logging.info(f"checkpointData: {checkpointData}")
+        finalized_epoch = int(checkpointData[1])
+        finalized_block_root = checkpointData[0]
     logging.debug(f'Got data from {ETH2_API} - '
                   'finalized epoch: {finalized_epoch}, '
                   'active val. count: {active_validator_count}, '
@@ -174,60 +179,11 @@ def update_ws_data_cache():
         "ws_period": ws_period,
         "ws_state": ws_state,
     }
-    current_epoch = get_current_epoch()
-    ws_data_cache = {
-        "caching_epoch": current_epoch,
-        "ws_data": ws_data
-    }
-    logging.info(
-        f"Updating redis cache key 'ws_data_cache' with {ws_data_cache}"
-    )
-    r.set('ws_data_cache', json.dumps(ws_data_cache))
     return ws_data
 
 
-def get_ws_data():
-    ws_data_cache_bytes = r.get('ws_data_cache')
-    # If redis cache is empty, initialize it
-    if ws_data_cache_bytes is None:
-        logging.debug(
-            "No 'ws_data_cache' in cache. Initializing cache now."
-        )
-        return update_ws_data_cache()
-    ws_data_cache = json.loads(ws_data_cache_bytes.decode('utf-8'))
-    logging.debug(
-        f"Got from redis cache for key 'ws_data_cache': {ws_data_cache}"
-    )
-    # Refresh redis cache if it has been more than MIN_CACHE_UPDATE_EPOCHS
-    # since last update
-    current_epoch = get_current_epoch()
-    cache_expired = (
-        current_epoch - ws_data_cache["caching_epoch"]
-        > MIN_CACHE_UPDATE_EPOCHS
-    )
-    if cache_expired:
-        logging.debug(
-            "Cached value for 'ws_data_cache' has expired - "
-            f"ws_data_cache: {ws_data_cache}, current epoch: {current_epoch}"
-        )
-        return update_ws_data_cache()
-    ws_data = ws_data_cache["ws_data"]
-    # Refresh redis cache if the current epoch is outside of the WS safety
-    # period since last update
-    current_epoch_in_ws_period = (
-        current_epoch - ws_data["finalized_epoch"] < ws_data["ws_period"]
-    )
-    if not current_epoch_in_ws_period:
-        logging.info(
-            "Cached value for 'ws_data_cache' is unsafe - "
-            f"ws_data_cache: {ws_data_cache}, current epoch: {current_epoch}"
-        )
-        return update_ws_data_cache()
-    return ws_data
-
-
-def prepare_response():
-    ws_data = get_ws_data()
+def prepare_response(checkpoint):
+    ws_data = get_ws_data(checkpoint)
     current_epoch = get_current_epoch()
     current_epoch_in_ws_period = (
         current_epoch - ws_data["finalized_epoch"] < ws_data["ws_period"]
@@ -247,7 +203,7 @@ def prepare_response():
 # Update redis cache on startup
 logging.info("Initializing cache. Server will be ready soon.")
 get_current_epoch()
-update_ws_data_cache()
+# get_ws_data()
 logging.info("Cache initialized. Ready to serve requests.")
 
 app = Flask(__name__)
@@ -255,4 +211,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def serve_ws_data():
-    return jsonify(prepare_response())
+    checkpoint = request.args.get('checkpoint')
+    return jsonify(prepare_response(checkpoint))
